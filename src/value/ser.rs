84b4e0dcfe155d1dc5353f42fc7180c664f18dee
taekwombo <88032359+taekwombo@users.noreply.github.com>
2021-02-28 12:00:00 +0100

    fmt

--- src/value/ser.rs
+++ src/value/ser.rs
@@ -5,27 +5,27 @@ use serde::ser::{self, SerializeMap, SerializeSeq, SerializeTupleStruct};
 impl ser::Serialize for Value {
     fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         match self {
-            Value::Null => serializer.serialize_unit(),
-            Value::Bool(v) => serializer.serialize_bool(*v),
-            Value::I64(v) => serializer.serialize_i64(*v),
-            Value::F64(v) => serializer.serialize_f64(*v),
-            Value::String(v) => serializer.serialize_str(&v),
-            Value::List(v) => {
+            Self::Null => serializer.serialize_unit(),
+            Self::Bool(v) => serializer.serialize_bool(*v),
+            Self::I64(v) => serializer.serialize_i64(*v),
+            Self::F64(v) => serializer.serialize_f64(*v),
+            Self::String(v) => serializer.serialize_str(&v),
+            Self::List(v) => {
                 let mut seq = serializer.serialize_seq(Some(v.len()))?;
                 for e in v {
                     seq.serialize_element(&e)?;
                 }
                 seq.end()
             }
-            Value::Map(v) => {
+            Self::Map(v) => {
                 let mut map = serializer.serialize_map(Some(v.len()))?;
                 for (k, v) in v.iter() {
                     map.serialize_entry(k, v)?;
                 }
                 map.end()
             }
-            Value::Bytes(v) => serializer.serialize_bytes(&*v),
-            Value::Structure { signature, fields } => {
+            Self::Bytes(v) => serializer.serialize_bytes(&*v),
+            Self::Structure { signature, fields } => {
                 let mut tuple = serializer.serialize_tuple_struct(STRUCTURE_NAME, fields.len())?;
                 tuple.serialize_field(signature)?;
                 for elem in fields.iter() {
