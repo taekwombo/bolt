bf46217d5e0e0c3d8336c9c3bdd875387b53f980
taekwombo <88032359+taekwombo@users.noreply.github.com>
2021-03-22 12:00:00 +0100

    Reexport all messages

--- src/types/message/ignored.rs
+++ src/types/message/ignored.rs
@@ -1,30 +1,33 @@
+use crate::constants::{STRUCTURE_FIELDS_KEY, STRUCTURE_NAME, STRUCTURE_SIG_KEY};
+use serde::{
+    de::{self, Error},
+    ser::{self, SerializeTupleStruct},
+};
 use std::fmt;
-use serde::{de::{self, Error}, ser::{self, SerializeTupleStruct}};
-use crate::constants::{STRUCTURE_NAME, STRUCTURE_SIG_KEY, STRUCTURE_FIELDS_KEY};
 
 const MSG_IGNORED_SIGNATURE: u8 = 0x7E;
 const MSG_IGNORED_LENGTH: u8 = 0x00;
-const MSG_IGNORED_SERIALIZE_LENGTH: usize = serialize_length!(MSG_IGNORED_SIGNATURE, MSG_IGNORED_LENGTH);
+const MSG_IGNORED_SERIALIZE_LENGTH: usize =
+    serialize_length!(MSG_IGNORED_SIGNATURE, MSG_IGNORED_LENGTH);
 
 #[derive(Debug, PartialEq)]
 pub struct Ignored;
 
 impl ser::Serialize for Ignored {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: ser::Serializer
+    where
+        S: ser::Serializer,
     {
         serializer
-            .serialize_tuple_struct(
-                STRUCTURE_NAME,
-                MSG_IGNORED_SERIALIZE_LENGTH,
-            )?
+            .serialize_tuple_struct(STRUCTURE_NAME, MSG_IGNORED_SERIALIZE_LENGTH)?
             .end()
     }
 }
 
 impl<'de> de::Deserialize<'de> for Ignored {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where D: de::Deserializer<'de>
+    where
+        D: de::Deserializer<'de>,
     {
         deserializer.deserialize_map(IgnoredVisitor)
     }
@@ -40,7 +43,8 @@ impl<'de> de::Visitor for IgnoredVisitor {
     }
 
     fn visit_map<V>(self, mut map_access: V) -> Result<Self::Value, V::Error>
-        where V: de::MapAccess<'de>
+    where
+        V: de::MapAccess<'de>,
     {
         match map_access.next_key::<&str>()? {
             Some(key) if key == STRUCTURE_SIG_KEY => {
@@ -51,7 +55,7 @@ impl<'de> de::Visitor for IgnoredVisitor {
                     key(),
                 });
                 Ok(Ignored)
-            },
+            }
             Some(key) => unexpected_key_access!(key),
             None => unexpected_key_access!(),
         }
@@ -61,28 +65,27 @@ impl<'de> de::Visitor for IgnoredVisitor {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{from_bytes, to_bytes, constants::marker::TINY_STRUCT};
+    use crate::{constants::marker::TINY_STRUCT, from_bytes, to_bytes};
 
     const BYTES: &'static [u8] = &[TINY_STRUCT, MSG_IGNORED_SIGNATURE];
 
     #[test]
-    fn serialize () {
+    fn serialize() {
         let result = to_bytes(&Ignored);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), BYTES);
     }
 
     #[test]
-    fn deserialize () {
+    fn deserialize() {
         let result = from_bytes::<Ignored>(BYTES);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), Ignored);
     }
 
     #[test]
-    fn deserialize_fail () {
+    fn deserialize_fail() {
         let result = from_bytes::<Ignored>(&[TINY_STRUCT, MSG_IGNORED_SIGNATURE + 1]);
         assert!(result.is_err());
     }
 }
-
--- src/types/message/reset.rs
+++ src/types/message/reset.rs
@@ -1,6 +1,9 @@
+use crate::constants::{STRUCTURE_FIELDS_KEY, STRUCTURE_NAME, STRUCTURE_SIG_KEY};
+use serde::{
+    de::{self, Error},
+    ser::{self, SerializeTupleStruct},
+};
 use std::fmt;
-use serde::{de::{self, Error}, ser::{self, SerializeTupleStruct}};
-use crate::constants::{STRUCTURE_NAME, STRUCTURE_SIG_KEY, STRUCTURE_FIELDS_KEY};
 
 const MSG_RESET_SIGNATURE: u8 = 0x0F;
 const MSG_RESET_LENGTH: u8 = 0x00;
@@ -11,20 +14,19 @@ pub struct Reset;
 
 impl ser::Serialize for Reset {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: ser::Serializer
+    where
+        S: ser::Serializer,
     {
         serializer
-            .serialize_tuple_struct(
-                STRUCTURE_NAME,
-                MSG_RESET_SERIALIZE_LENGTH,
-            )?
+            .serialize_tuple_struct(STRUCTURE_NAME, MSG_RESET_SERIALIZE_LENGTH)?
             .end()
     }
 }
 
 impl<'de> de::Deserialize<'de> for Reset {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> 
-        where D: de::Deserializer<'de>
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
     {
         deserializer.deserialize_map(ResetVisitor)
     }
@@ -40,7 +42,8 @@ impl<'de> de::Visitor<'de> for ResetVisitor {
     }
 
     fn visit_map<V>(self, mut map_access: V) -> Result<Self::Value, V::Error>
-        where V: de::MapAccess<'de>
+    where
+        V: de::MapAccess<'de>,
     {
         match map_access.next_key::<&str>()? {
             Some(key) if key == STRUCTURE_NAME => {
@@ -51,7 +54,7 @@ impl<'de> de::Visitor<'de> for ResetVisitor {
                     key(),
                 });
                 Ok(Reset)
-            },
+            }
             Some(key) => unexpected_key_access!(key),
             None => unexpected_key_access!(),
         }
@@ -61,28 +64,27 @@ impl<'de> de::Visitor<'de> for ResetVisitor {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{from_bytes, to_bytes, constants::marker::TINY_STRUCT};
+    use crate::{constants::marker::TINY_STRUCT, from_bytes, to_bytes};
 
     const BYTES: &'static [u8] = &[TINY_STRUCT, MSG_RESET_SIGNATURE];
 
     #[test]
-    fn serialize () {
+    fn serialize() {
         let result = to_bytes(&Reset);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), BYTES);
     }
 
     #[test]
-    fn deserialize () {
+    fn deserialize() {
         let result = from_bytes::<Reset>(BYTES);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), Reset);
     }
 
     #[test]
-    fn deserialize_fail () {
+    fn deserialize_fail() {
         let result = from_bytes::<Reset>(&[TINY_STRUCT, MSG_RESET_SIGNATURE + 1]);
         assert!(result.is_err());
     }
 }
-
