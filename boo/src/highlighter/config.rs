3c1ad89cd72e7f8ef59199e90fd91e0c33351946
taekwombo <88032359+taekwombo@users.noreply.github.com>
2022-04-08 18:26:57 +0200

    chore: add comments, update highlighting

--- boo/src/highlighter/config.rs
+++ boo/src/highlighter/config.rs
@@ -2,12 +2,16 @@ use termion::color;
 
 static HIGHLIGHT_GROUPS: &'static [&'static str] = &[
     "keyword",
+    "number",
+    "comment",
 ];
 
 static EMPTY: &'static str = "";
 
 pub struct HighlightColors {
     keyword: &'static str,
+    number: &'static str,
+    gray: &'static str,
     reset: &'static str,
 }
 
@@ -23,9 +27,13 @@ impl HighlightColors {
     pub fn new() -> Self {
         let yellow: &'static str = color::Yellow.fg_str();
         let reset: &'static str = color::Reset.fg_str();
+        let number: &'static str = color::Blue.fg_str();
+        let gray: &'static str = Box::leak(color::AnsiValue::grayscale(20).fg_string().into_boxed_str());
 
         Self {
             reset,
+            number,
+            gray,
             keyword: yellow,
         }
     }
@@ -33,6 +41,8 @@ impl HighlightColors {
     pub fn get_color(&self, highlight_index: usize) -> &'static str {
         match highlight_index {
             0 => self.keyword,
+            1 => self.number,
+            2 => self.gray,
             _ => EMPTY,
         }
     }
